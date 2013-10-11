4870f6ee8358cd441fdd39599912da6a68af5a2b
fengyiliu <liufengyi2006123@sina.com>
2013-10-11 22:21:52 +0800

    sfsdfdsf

--- www/js/index.js
+++ www/js/index.js
@@ -43,7 +43,7 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-				return;
+			
 
         location.href="http://1.weishequserver.duapp.com/";
 
